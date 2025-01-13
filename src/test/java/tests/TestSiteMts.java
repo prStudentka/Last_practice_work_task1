1dbd69d48f2bf038091b88bbfb61176e3c640f36
Irina <135240986+prStudentka@users.noreply.github.com>
2025-01-13 12:47:57 +0300

    Update TestSiteMts.java

--- src/test/java/tests/TestSiteMts.java
+++ src/test/java/tests/TestSiteMts.java
@@ -2,7 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -15,12 +15,16 @@ public class TestSiteMts {
 
     @BeforeAll
     public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+        System.setProperty("chromeoptions.args", "\"--no-sandbox\",\"--disable-dev-shm-usage\"");
         mainPage = new MainPage();
     }
     
     @AfterAll
     public static void tearDown() {
+        Configuration.browser = "chrome";
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
     }
